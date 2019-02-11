5996b41c50e3c204ce7213adbd348a203271950e
GreatHayat <greathayat@gmail.com>
2019-02-11 21:34:51 +0500

    Add statich files with bootstrap styles

--- boards/tests.py
+++ boards/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.urls import resolve, reverse
+from . import views
+from . import models
 # Create your tests here.
+class HomeTests(TestCase):
+    def test_index_view(self):
+        c = Client()
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_view_url_resolve_index(self):
+        view = resolve("/")
+        self.assertEqual(view.func, views.index)
